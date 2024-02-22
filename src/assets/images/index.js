d2ee76e44ba448b2c093853f12ce8ae992e9981d
Molise Mahasele <molisemahasele@yahoo.com>
2024-02-21 18:31:54 -0800

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,46 @@
+import bigShoe1 from './qqq.png'
+import bigShoe2 from './ddd.png'
+import bigShoe3 from './eee.png'
+
+import customer1 from './customer1.jpeg'
+import customer2 from './customer2.svg'
+
+import footerLogo from './JOACHIM.png'
+import headerLogo from './rr.png'
+
+import offer from './jjj.jpg'
+
+import shoe4 from './12.png'
+import shoe5 from './13.png'
+import shoe6 from './14.png'
+import shoe7 from './g.png'
+import shoe8 from './13.png'
+
+import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailShoe1 from './hhh.jpg'
+import thumbnailShoe2 from './g.png'
+import thumbnailShoe3 from './ffff.png'
+
+export {
+    bigShoe1,
+    bigShoe2,
+    bigShoe3,
+
+    customer1,
+    customer2,
+
+    footerLogo,
+    headerLogo,
+
+    offer,
+    shoe4,
+    shoe5,
+    shoe6,
+    shoe7,
+    shoe8,
+
+    thumbnailBackground,
+    thumbnailShoe1,
+    thumbnailShoe2,
+    thumbnailShoe3
+}
